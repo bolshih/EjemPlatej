2a3b985622b895252a8cf8c456ca1de7f27e69b3
boshih <artem.bolshih@mail.ru>
2022-05-25 21:44:30 +0300

    start

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,16 @@
+public class CreditPaymentService {
+    public double calculate(double o, double pp, double s){
+        double pc;
+        double x;
+        double a;
+        double y;
+        double p;
+
+        pc = s / (100 * 12);
+        x = 1 + pc;
+        a = -pp;
+        y = Math.pow(x,a);
+        p = o * pc / (1-y);
+        return p;
+    }
+}
